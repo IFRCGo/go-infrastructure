78cf50b74127e27baf683172a969310a6d50c968
Derek Lieu <dl@dereklieu.com>
2017-11-20 22:33:22 -0800

    Create scripts to open and close ssh

---
+++ vms/open-ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# open port 22 for SSH traffic
+GO_NSG="dsgoNSG"
+az network nsg rule create \
+  --resource-group $RESOURCE_GROUP \
+  --nsg-name $GO_NSG \
+  --name 'ssh' \
+  --access allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 1000 \
+  --source-address-prefix "Internet" \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range 22
