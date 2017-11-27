cef1b0e9ef29783ae85ec6be082b00cb29e842ea
Derek Lieu <dl@dereklieu.com>
2017-11-26 19:22:02 -0500

    Set tippecanoe to create tile levels

--- tiles/make-tiles.sh
+++ tiles/make-tiles.sh
@@ -7,10 +7,13 @@ mkdir -p $TMP_DIR
 
 COUNTRY_SHP=tiles/input/natural-earth-shapefiles/ne_10m_admin_0_countries_lakes/ne_10m_admin_0_countries_lakes.shp
 POPULATION_SHP=tiles/input/natural-earth-shapefiles/ne_10m_populated_places_simple/ne_10m_populated_places_simple.shp
-#SIMPLIFICATION=(100 50 10 5)
-#POPULATION_THRESHOLDS=(1000 10000 100000 5000000)
-SIMPLIFICATION=(5)
-POPULATION_THRESHOLDS=(5000000)
+POPULATION_THRESHOLDS=(5000000 500000 10000 1000)
+SIMPLIFICATION=(5 10 50 100)
+MIN_ZOOMS=(1 4 6 8)
+MAX_ZOOMS=(3 5 7 10)
+
+# Debugging with one simplification level
+# SIMPLIFICATION=(5)
 
 
 echo ""
@@ -35,6 +38,9 @@ for i in ${!SIMPLIFICATION[@]}; do
   PCT_SIMPLIFICATION=${SIMPLIFICATION[$i]}
   DIR=$TMP_DIR/$PCT_SIMPLIFICATION
   POPULATION_THRESHOLD=${POPULATION_THRESHOLDS[$i]}
+  cat $DIR/population.geojson \
+    | ./bin/filter-by-population --threshold $POPULATION_THRESHOLD \
+    > $DIR/filtered-population.geojson
 done
 
 
@@ -47,7 +53,7 @@ for PCT_SIMPLIFICATION in ${SIMPLIFICATION[@]}; do
     | node_modules/.bin/dirty-reproject --forward robinson \
     > $DIR/robinson-country.geojson
 
-  cat $DIR/population.geojson \
+  cat $DIR/filtered-population.geojson \
     | node_modules/.bin/dirty-reproject --forward robinson \
     > $DIR/robinson-population.geojson
 done
@@ -55,17 +61,24 @@ done
 
 echo ""
 echo "Generating tiles"
+for i in ${!SIMPLIFICATION[@]}; do
+  PCT_SIMPLIFICATION=${SIMPLIFICATION[$i]}
+  DIR=$TMP_DIR/$PCT_SIMPLIFICATION
+  OUTPUT_DIR=$TMP_DIR/output/$PCT_SIMPLIFICATION
+  mkdir -p $OUTPUT_DIR
+  MIN_ZOOM=${MIN_ZOOMS[$i]}
+  MAX_ZOOM=${MAX_ZOOMS[$i]}
 
-OUTPUT_DIR=$TMP_DIR/output
-mkdir -p $OUTPUT_DIR
+  echo $MIN_ZOOM
+  echo $MAX_ZOOM
 
-tippecanoe --projection EPSG:3857 \
-  --named-layer=country:$TMP_DIR/5/robinson-country.geojson \
-  --named-layer=population:$TMP_DIR/5/robinson-population.geojson \
-  --read-parallel \
-  --no-polygon-splitting \
-  --drop-rate=0 \
-  --output-to-directory=$OUTPUT_DIR \
-  --no-tile-compression \
-  --maximum-zoom=1 \
-  --minimum-zoom=3
+  tippecanoe --projection EPSG:4326 \
+    --named-layer=country:$DIR/robinson-country.geojson \
+    --named-layer=population:$DIR/robinson-population.geojson \
+    --drop-rate=0 \
+    --output-to-directory=$OUTPUT_DIR \
+    --no-tile-compression \
+    --maximum-zoom=$MAX_ZOOM \
+    --minimum-zoom=$MIN_ZOOM
+
+done
