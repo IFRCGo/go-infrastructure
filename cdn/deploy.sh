16875cbfe978a24f3dad634cc9db3e4217ebed9b
Derek Lieu <dl@dereklieu.com>
2018-01-09 00:18:12 -0500

    Include cdn deploy

---
+++ cdn/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+FQDN="$(az network public-ip show -g $RESOURCE_GROUP -n $API_IP_NAME --query "{ fqdn: dnsSettings.fqdn }" | jq -r '.fqdn')"
+
+
+az cdn profile create --name $CDN_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --location $REGION \
+  --sku Standard_Verizon
+
+
+az cdn endpoint create --name $CDN_API_ENDPOINT_NAME \
+  --origin $FQDN 80 443 \
+  --profile-name $CDN_NAME \
+  --resource-group $RESOURCE_GROUP
