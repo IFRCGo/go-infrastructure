f1bb2e9ea4d24c7e3115951f931582d676334826
Derek Lieu <dl@dereklieu.com>
2018-01-24 17:03:52 -0500

    Create metric notifications for db usage

--- db/deploy.sh
+++ db/deploy.sh
@@ -27,3 +27,33 @@ az postgres server firewall-rule create \
   --name AllowLocalMachine \
   --start-ip-address $LOCAL_IP \
   --end-ip-address $LOCAL_IP
+
+# Create metric notification rules
+DB_APP_ID=$(az resource show -g $RESOURCE_GROUP -n $DB_SERVER_NAME --resource-type "Microsoft.DBforPostgreSQL/servers" --query id --output tsv)
+
+az monitor alert create \
+  --name CPUUsage \
+  --resource-group $RESOURCE_GROUP \
+  --target $DB_APP_ID \
+  --condition "cpu_percent > 75 avg 5m" \
+  --description "Emails administrators when CPU usage exceeds 75% average for 5 minutes" \
+  --email-service-owners \
+  --action email derek@developmentseed.org
+
+az monitor alert create \
+  --name ComputeUsage \
+  --resource-group $RESOURCE_GROUP \
+  --target $DB_APP_ID \
+  --condition "compute_limit > 75 avg 5m" \
+  --description "Emails administrators when compute limit exceeds 75% average for 5 minutes" \
+  --email-service-owners \
+  --action email derek@developmentseed.org
+
+az monitor alert create \
+  --name IOConsumption \
+  --resource-group $RESOURCE_GROUP \
+  --target $DB_APP_ID \
+  --condition "io_consumption_percent > 75 avg 5m" \
+  --description "Emails administrators when IO consumption exceeds 75% average for 5 minutes" \
+  --email-service-owners \
+  --action email derek@developmentseed.org
