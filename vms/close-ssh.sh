48b3fe35a224dd5b6fe9ea764bc76701dde40725
Derek Lieu <dl@dereklieu.com>
2018-06-14 11:14:11 +0100

    Update vm scripts

--- vms/close-ssh.sh
+++ vms/close-ssh.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # close port 22, disable ssh
-GO_NSG="dsgoNSG"
 az network nsg rule delete \
   --resource-group $RESOURCE_GROUP \
   --nsg-name $GO_NSG \
--- vms/init-es.sh
+++ vms/init-es.sh
@@ -13,6 +13,10 @@ chmod 600 .tmp/key
 
 IP="$(az vm list-ip-addresses --resource-group $RESOURCE_GROUP --name $ES_NAME --query [0].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv)"
 
-ssh -i .tmp/key -o StrictHostKeychecking=no $API_ADMIN@$IP docker run -d -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch-oss:6.0.0
+ssh -i .tmp/key -o StrictHostKeychecking=no $API_ADMIN@$IP /bin/bash << EOF
+  docker stop \$(docker ps -q)
+  docker rm \$(docker ps -a -q)
+  docker run -d -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch-oss:6.0.0
+EOF
 
 rm .tmp/key
