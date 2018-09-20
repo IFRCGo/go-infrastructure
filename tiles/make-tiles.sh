52e89c369c27b3c0ec725819794c432ce9bad89c
Derek Lieu <dl@dereklieu.com>
2018-09-20 11:33:34 -0400

    Sync new tile work

--- tiles/make-tiles.sh
+++ tiles/make-tiles.sh
@@ -8,15 +8,15 @@ TMP_DIR=.tmp/tiles
 rm -rf $TMP_DIR
 mkdir -p $TMP_DIR
 
-COUNTRY_SHP=tiles/input/ne_10m_admin_0_countries_lakes/ne_10m_admin_0_countries_lakes.shp
+COUNTRY_SHP=tiles/input/ICRC_Admin0_WD_Internal/ICRC_Admin0_WD_Internal.shp
+BOUNDARY_SHP=tiles/input/ICRC_Admin0_Borders_WD_Internal/ICRC_Admin0_Borders_WD_Internal.shp
 ADM0_SHP=tiles/input/ICRC_Admin1_WD_Internal/ICRC_Admin1_WD_Internal.shp
-SIMPLIFICATION=(5 10 50 90)
+SIMPLIFICATION=(4 8 32 80)
 MIN_ZOOMS=(1 4 6 8)
-MAX_ZOOMS=(3 5 7 12)
+MAX_ZOOMS=(3 5 7 10)
 
 # Debugging with one simplification level
-# SIMPLIFICATION=(5)
-
+#SIMPLIFICATION=(50)
 
 echo ""
 echo "Creating simplified geojson files"
@@ -26,12 +26,21 @@ for PCT_SIMPLIFICATION in ${SIMPLIFICATION[@]}; do
 
   node_modules/.bin/mapshaper $COUNTRY_SHP \
     -simplify visvalingam $PCT_SIMPLIFICATION% \
+    -proj wgs84 \
     -filter-islands min-vertices=2 remove-empty \
-    -filter-fields ISO_A2,ADM0_A3_IS,NAME,ADM0_A3 \
+    -filter-fields ISO2,ISO3,NAME,NAME_ICRC \
     -o format=geojson $DIR/country.geojson
+
+  node_modules/.bin/mapshaper $BOUNDARY_SHP \
+    -simplify visvalingam $PCT_SIMPLIFICATION% \
+    -proj wgs84 \
+    -filter-islands min-vertices=2 remove-empty \
+    -filter-fields OBJECTID,TYPE,TYPE2 \
+    -o format=geojson $DIR/boundary.geojson
 
   node_modules/.bin/mapshaper $ADM0_SHP \
     -simplify visvalingam $PCT_SIMPLIFICATION% \
+    -proj wgs84 \
     -filter-islands min-vertices=2 remove-empty \
     -filter-fields ISO2,Admin00Nam,Admin01Nam,OBJECTID,Admin01Cod \
     -o format=geojson $DIR/adm1.geojson
@@ -39,8 +48,8 @@ done
 
 echo ""
 echo "Creating centroid file"
-cat $TMP_DIR/50/country.geojson | ./bin/centroid > $CENTROIDS_DIR/country-centroids.geojson
-cat $TMP_DIR/50/adm1.geojson | ./bin/centroid > $CENTROIDS_DIR/adm1-centroids.geojson
+cat $TMP_DIR/${SIMPLIFICATION[0]}/country.geojson | ./bin/centroid > $CENTROIDS_DIR/country-centroids.geojson
+cat $TMP_DIR/${SIMPLIFICATION[0]}/adm1.geojson | ./bin/centroid > $CENTROIDS_DIR/adm1-centroids.geojson
 
 
 echo ""
@@ -56,9 +65,10 @@ for i in ${!SIMPLIFICATION[@]}; do
   echo $MIN_ZOOM
   echo $MAX_ZOOM
 
-  tippecanoe --projection EPSG:3857 \
+  tippecanoe --projection EPSG:4326 \
     --named-layer=country:$SOURCE_DIR/country.geojson \
     --named-layer=adm1:$SOURCE_DIR/adm1.geojson \
+    --named-layer=boundary:$SOURCE_DIR/boundary.geojson \
     --named-layer=country-centroids:$CENTROIDS_DIR/country-centroids.geojson \
     --named-layer=adm1-centroids:$CENTROIDS_DIR/adm1-centroids.geojson \
     --read-parallel \
