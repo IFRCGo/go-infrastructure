04b6202fecaecbb185edbd601600b9f0c2050f24
Szabó, Zoltán <szabozoltan69@gmail.com>
2021-04-23 17:07:52 +0200

    Add files via upload

---
+++ vms/check_init-api.bash
@@ -0,0 +1,12 @@
+cut -d'"' -f2 init-api.sh|grep '\$'|grep -v scp|grep -v ssh|grep -v '^ '|grep -v "(" |\
+grep -v AZURE_STORAGE_ACCOUNT |\
+grep -v AZURE_STORAGE_KEY |\
+grep -v API_FQDN > check0
+
+grep -o =.*=  check0
+grep ^'\$' check0
+
+cut -d'=' -f1 check0 > check1
+cut -d'=' -f2 check0|grep '\$'|cut -c 2- > check2
+diff check1 check2
+rm check?
