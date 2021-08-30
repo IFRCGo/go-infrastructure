8124af7b9ded51d1ba25e4fd60db149b3cf0fd21
Szabó, Zoltán <szabozoltan69@gmail.com>
2021-08-30 16:21:17 +0200

    Add files via upload

---
+++ bin/process_orig_svg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+fout    =  open("ids_in.svg","w")
+finp    =  open("orig.svg")
+i       = 1
+text    = ''
+
+for index,line in enumerate(finp):
+    x = line.strip().split(" ")
+    if x[0]=='<rect':
+        x[0]+= ' id=' + str(i) + ' className="svgPointerEvent" opacity="0.8"'
+        i += 1
+        text += ' '.join(x) + "\n"
+    else:
+        text += line
+
+print (text, file = fout)
+
+fout.close()
+finp.close()
