02ae8326aa58bfb894804c39c1d5444cb92b3788
Szabó, Zoltán <szabozoltan69@gmail.com>
2021-09-22 11:40:09 +0200

    Create collectResults.sh

---
+++ checkBrokenLinks/collectResults.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -f                                                                  nslinks
+wget 'https://prddsgocdnapi.azureedge.net/api/v2/nslinks?limit=200' -O nslinks
+perl -pi -w -e 's/\}/\n/g'                                             nslinks
+perl -pi -w -e 's/^.*url_ifrc...//;s/^.\n//;s/"//'                     nslinks
+
+cd results
+rm -f *
+cat ../nslinks|xargs -iQ sh -c "curl -s Q|grep 'h2 class=.page-hero' > \$(echo Q|sed 's/^.*\///')"
+
+# Example: https://www.ifrc.org/national-societies-directory/bangladesh-red-crescent-society
